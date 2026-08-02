c03b3f8f45022970be7a443fd8d62044a626e7d7
TheGwiroch <colathefirst@gmail.com>
2026-08-02 17:41:50 +0300

    Add in-memory monster repository

---
+++ src/main/java/com/arsbog/monsterapi/repository/MonsterRepository.java
@@ -0,0 +1,29 @@
+package com.arsbog.monsterapi.repository;
+
+import com.arsbog.monsterapi.model.Monster;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Optional;
+
+@Repository
+public class MonsterRepository {
+    private long nextId = 1;
+    public Monster save(Monster monster){
+    monster.setId(nextId++);
+    monsters.add(monster);
+    return monster;
+    }
+    private final List<Monster> monsters = new ArrayList<>();
+    public List<Monster> findAll() {
+        return monsters;
+    }
+    public Optional<Monster> findById(Long id){
+        return monsters.stream().filter(monster -> monster.getId().equals(id)).findFirst();
+    }
+    public boolean deleteById(Long id){
+        return monsters.removeIf(monster -> monster.getId().equals(id));
+    }
+}
