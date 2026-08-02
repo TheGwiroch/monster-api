d1c68342acfd3f3204f2960b29ca9909f75bf181
TheGwiroch <colathefirst@gmail.com>
2026-08-02 18:51:42 +0300

    Eddited MonsterController

--- src/main/java/com/arsbog/monsterapi/controller/MonsterController.java
+++ src/main/java/com/arsbog/monsterapi/controller/MonsterController.java
@@ -2,10 +2,7 @@ package com.arsbog.monsterapi.controller;
 
 import com.arsbog.monsterapi.model.Monster;
 import com.arsbog.monsterapi.service.MonsterService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,7 @@ public class MonsterController {
     }
 
     @PostMapping
-    public Monster create(Monster monster) {
+    public Monster create(@RequestBody Monster monster) {
         return service.create(monster);
     }
 
