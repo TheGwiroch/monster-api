66d2b24970b5bfa2acd9042ea58c79c944b67ffe
TheGwiroch <colathefirst@gmail.com>
2026-08-02 18:37:20 +0300

    Added MonsterService

---
+++ src/main/java/com/arsbog/monsterapi/service/MonsterService.java
@@ -0,0 +1,29 @@
+package com.arsbog.monsterapi.service;
+
+import com.arsbog.monsterapi.model.Monster;
+import com.arsbog.monsterapi.repository.MonsterRepository;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MonsterService {
+    private final MonsterRepository repository;
+
+    public MonsterService(MonsterRepository repository) {
+        this.repository = repository;
+    }
+    public List<Monster> findAll(){
+        return repository.findAll();
+    }
+    public Optional<Monster> findById (Long id){
+        return repository.findById(id);
+    }
+    public Monster create(Monster monster){
+        return repository.save(monster);
+    }
+    public boolean deleteById(Long id){
+        return repository.deleteById(id);
+    }
+}
