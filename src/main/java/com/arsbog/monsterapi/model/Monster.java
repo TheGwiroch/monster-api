01729e9acbec6077d462aaa7d7268692bfbe11fb
TheGwiroch <colathefirst@gmail.com>
2026-08-02 16:11:59 +0300

    Initialize project and Monster model

---
+++ src/main/java/com/arsbog/monsterapi/model/Monster.java
@@ -0,0 +1,9 @@
+package com.arsbog.monsterapi.model;
+
+public class Monster {
+    private Long id;
+    private String name;
+    private String type;
+    private int power;
+    private int health;
+}
---
+++ src/test/java/com/arsbog/monsterapi/MonsterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsbog.monsterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
