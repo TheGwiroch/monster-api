b1ab023881e8989c20326951830419c6d1c9e741
TheGwiroch <colathefirst@gmail.com>
2026-08-03 16:45:59 +0300

    Udpdated MonsterController

--- src/main/java/com/arsbog/monsterapi/controller/MonsterController.java
+++ src/main/java/com/arsbog/monsterapi/controller/MonsterController.java
@@ -3,6 +3,7 @@ package com.arsbog.monsterapi.controller;
 import com.arsbog.monsterapi.model.Monster;
 import com.arsbog.monsterapi.service.MonsterService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,9 +26,10 @@ public class MonsterController {
     public Monster create(@RequestBody Monster monster) {
         return service.create(monster);
     }
+
     @GetMapping("/{id}")
-    public Optional<Monster> findById(@PathVariable Long id){
-        return service.findById(id);
+    public ResponseEntity<Monster> findById(@PathVariable Long id) {
+        return service.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
